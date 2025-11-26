a5b302542e3d5c6347ccbc4934e1f9189ed0728f
Yazouv <yazouv.pro@gmail.com>
2025-11-26 09:38:02 +0100

    Refactor add Constants for Position.java

--- src/main/java/fr/unilim/Position.java
+++ src/main/java/fr/unilim/Position.java
@@ -2,20 +2,23 @@ package fr.unilim;
 
 public record Position(int x, int y) {
 
+    private static final int INCREMENT = 1;
+    private static final int DECREMENT = -1;
+
     public Position moveNorth() {
-        return new Position(x, y + 1);
+        return new Position(x, y + INCREMENT);
     }
 
     public Position moveSouth() {
-        return new Position(x, y - 1);
+        return new Position(x, y + DECREMENT);
     }
 
     public Position moveEast() {
-        return new Position(x + 1, y);
+        return new Position(x + INCREMENT, y);
     }
 
     public Position moveWest() {
-        return new Position(x - 1, y);
+        return new Position(x + DECREMENT, y);
     }
 
     @Override
